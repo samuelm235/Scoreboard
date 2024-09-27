79b865cbb4489818079074f66e17af1924f1462e
samuelm235 <108957962+samuelm235@users.noreply.github.com>
2024-09-27 09:01:34 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String info;
+        Scoreboard game = new Scoreboard("Red", "Blue");
+        game.recordPlay(1);
+        info = game.getScore();
+        System.out.println(info);
+
+    }
+}
---
+++ Scoreboard.java
@@ -0,0 +1,24 @@
+public class Scoreboard
+{
+    private String teamOne;
+    private String teamTwo;
+    private int teamOneScore;
+    private int teamTwoScore;
+    private String activeTeam;
+    public Scoreboard(String one, String two)
+    {
+        teamOne = one;
+        teamTwo = two;
+        activeTeam = teamOne;
+    }
+
+    public String getScore()
+    {
+        return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+    }
+
+    public void recordPlay(int score)
+    {
+    }
+
+}
